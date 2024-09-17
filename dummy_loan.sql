d2c82f376394c729294981885b88fb738a5c467f
Juuso Kekonen <juuso.kekonen@protonmail.com>
2024-09-17 14:14:29 +0300

    Temporary database files
    TODO: Stop relying on these pieces of shit

---
+++ dummy_loan.sql
@@ -0,0 +1,12 @@
+use `loans_db`;
+insert into `loans` (
+    CustomerID,
+    DeviceID,
+    LoanStart,
+    LoanEnd
+) values (
+	'1',
+    '2',
+    '2024-09-17',
+	'2024-09-18'
+);
---
+++ init_dummy.sql
@@ -0,0 +1,89 @@
+use `loans_db`;
+
+insert into `customers` (
+    `LastName`,
+    `FirstName`,
+    `Email`,
+    `Telephone`
+) values (
+	'Esimerkki',
+    'Erkki',
+    'erkki.esimerkki@mail.invalid',
+	'0401234567'
+);
+
+insert into `devices` (
+	`DeviceName`,
+    `DeviceType`,
+    `LoanStatus`
+) values (
+	'Esimerkkilaite',
+    'Esimerkkityyppi',
+	'0'
+);
+
+insert into `customers` (
+    `LastName`,
+    `FirstName`,
+    `Email`,
+    `Telephone`
+) values (
+	'Esimerkki',
+    'Elli',
+    'elli.esimerkki@mail.invalid',
+	'0401234567'
+);
+
+insert into `devices` (
+	`DeviceName`,
+    `DeviceType`,
+    `LoanStatus`
+) values (
+	'Esimerkkilaite 2',
+    'Esimerkkityyppi 2',
+	'0'
+);
+
+insert into `customers` (
+    `LastName`,
+    `FirstName`,
+    `Email`,
+    `Telephone`
+) values (
+	'Example',
+    'Eugene',
+    'eugene.example@mail.invalid',
+	'0401234567'
+);
+
+insert into `devices` (
+	`DeviceName`,
+    `DeviceType`,
+    `LoanStatus`
+) values (
+	'Example Device',
+    'Example Type',
+	'0'
+);
+
+insert into `customers` (
+    `LastName`,
+    `FirstName`,
+    `Email`,
+    `Telephone`
+) values (
+	'Example',
+    'Ella',
+    'ella.example@mail.invalid',
+	'0401234567'
+);
+
+insert into `devices` (
+	`DeviceName`,
+    `DeviceType`,
+    `LoanStatus`
+) values (
+	'Example Device 2',
+    'Example Type 2',
+	'0'
+);
